ad4f3b3b66048b78ebd68ade6985aa60fb1686b7
Daksh <dakshnahar2001@gmail.com>
2023-11-27 17:46:26 +0530

    Added: UC5 implementing monthly wage

--- employeeWage.java
+++ employeeWage.java
@@ -4,6 +4,8 @@ import java.util.Scanner;
 public class employeeWage {
     private static final int fullTime = 1;
     private static final int partTime = 2;
+    private static final int monthly = 3;
+    private static final int wageRate = 8;
     /*
         @desc: function gives random value for employee present or absent
         @params: none
@@ -25,30 +27,32 @@ public class employeeWage {
 
     public static void main(String[] arg){
         System.out.println("Employee Wage System");
-        System.out.print("Enter your choice(1. for full time, 2. for part time): ");
+        System.out.print("Enter your choice(1. for full time, 2. for part time, 3. for monthly): ");
         Scanner input = new Scanner(System.in);
         int choice = input.nextInt();
 
         switch (choice){
             case fullTime:
                 int attendance = presentOrAbsent();
-                int fullTimeWageRate = 8;
                 int fullTimeHours = 0;
                 if(attendance==1){
                     fullTimeHours = 20;
                     System.out.println("Employee is present");
                 }
-                System.out.println("Total Wage: " + totalWage(fullTimeWageRate,fullTimeHours));
+                System.out.println("Total Wage: " + totalWage(wageRate,fullTimeHours));
                 break;
             case partTime:
                 int attendence = presentOrAbsent();
                 int partTimeHours = 0;
-                int partTimeWageRate = 20;
                 if(attendence==1){
                     partTimeHours = 4;
                     System.out.println("Employee is present");
                 }
-                System.out.println("Total Wage: " + totalWage(partTimeWageRate,partTimeHours));
+                System.out.println("Total Wage: " + totalWage(wageRate,partTimeHours));
+                break;
+            case monthly:
+                int monthlyHours = 100;
+                System.out.println("Total Wage: " + totalWage(wageRate,monthlyHours));
                 break;
             default:
                 System.out.print("Invalid input");
